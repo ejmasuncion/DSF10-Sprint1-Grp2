fa0e6f163611495eed9748dbb95b8ff1a9856fe1
Enrico Asuncion <ejmasuncion@yahoo.com>
2022-09-23 19:53:26 +0800

    added has_mobileowner and fin5

--- DataViz/digital_access.py
+++ DataViz/digital_access.py
@@ -2,17 +2,20 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import plotly.express as px
 import seaborn as sns
+import numpy as np
 
 
 def digital_access(data):
-    data['fin14'] = data['fin14a'] + data['fin14b']
-    data['has_digital access'] = data['fin14'].apply(
-    lambda x:1 if (x >= 1 and x <= 3) else 0
-    )
+    data['has_digital access'] = np.where(data[['fin14a', 'fin14b', 'fin5']].eq(1).any(axis = 1 ), 1, 0)
+    # data['fin14'] = data['fin14a'] + data['fin14b']
+    # data['has_digital access'] = data['fin14'].apply(
+    # lambda x:1 if (x >= 1 and x <= 3) else 0
+    # )
 
-    data['Digital Access'] = data['has_digital access'].apply(
-        lambda x:'Yes' if (x >= 1 and x <= 3) else 'No'
-    )
+    # data['Digital Access'] = data['has_digital access'].apply(
+    #     lambda x:'Yes' if (x >= 1 and x <= 3) else 'No'
+    # )
+
     PH_data = data[data['economy']=='Philippines']
 
     fig, ax = plt.subplots(figsize=(10, 5))
@@ -29,12 +32,7 @@ def digital_access(data):
     return fig
 
 def digital_access_sea(data):
-    data['fin14'] = data['fin14a'] + data['fin14b']
-
-    #Filter mobile/internet access (FI access)
-    data['has_digital access'] = data['fin14'].apply(
-        lambda x:1 if (x >= 1 and x <= 3) else 0
-    )
+    data['has_digital access'] = np.where(data[['fin14a', 'fin14b', 'fin5']].eq(1).any(axis = 1 ), 1, 0)
 
     digital_grouped_data = data.groupby(['economy','regionwb']).agg(
     with_digitalaccess=('has_digital access', 'sum'),
@@ -60,3 +58,26 @@ def digital_access_sea(data):
     plt.xticks(rotation=45)
         
     return figure
+
+def has_mobile_phone(data):
+    PH_data = data[data['economy']=='Philippines']
+    PH_data['has_mobilephone'] = np.where(PH_data[['mobileowner']].eq(1).any(axis = 1 ), 1, 0)
+
+    fig, ax = plt.subplots(figsize=(10, 5))
+    sns.countplot(x='has_mobilephone', data=PH_data)
+    # Annotate percentage
+    total = len(PH_data['has_mobilephone'])
+    for p in ax.patches:
+        percentage = '{:.1f}%'.format(100 * p.get_height()/total)
+        x = p.get_x() + p.get_width() - 0.5
+        y = p.get_height() + 0.5
+        ax.annotate(percentage, (x, y))
+    
+    # Set labels
+    plt.xlabel('Mobile Phone')
+    plt.ylabel('Frequency')
+
+    return fig
+
+
+
